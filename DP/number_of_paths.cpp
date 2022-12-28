a6558c6e39c96b2d2917764bdf480f80ef7c491b
Ajay_Wakodikar <ajaywakodikar10@gmail.com>
2022-12-28 14:12:56 +0530

    Number of paths to reach from top left to bottom right using only down and right (updated)

--- DP/number_of_paths.cpp
+++ DP/number_of_paths.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
 
-long long number_of_paths(int M, int N, std::vector<std::vector<int>>& v) {
+long long number_of_paths(int M, int N) {
     if(M == 1 || N == 1) return 1;
+    static std::vector<std::vector<int>> v(M + 1, std::vector<int>(N + 1, -1));
     if(v[M][N] != -1) return v[M][N];
-    v[M - 1][N] = number_of_paths(M - 1, N, v);
-    v[M][N - 1] = number_of_paths(M, N - 1, v);
+    v[M - 1][N] = number_of_paths(M - 1, N);
+    v[M][N - 1] = number_of_paths(M, N - 1);
     v[M][N] = v[M - 1][N] + v[M][N - 1];
     return v[M][N];
 }
@@ -13,7 +14,6 @@ long long number_of_paths(int M, int N, std::vector<std::vector<int>>& v) {
 int main() {
     int M, N;
     std::cin >> M >> N;
-    std::vector<std::vector<int>> v(M + 1, std::vector<int>(N + 1, -1));
-    long long result = number_of_paths(M, N, v);
+    long long result = number_of_paths(M, N);
     std::cout << result << "\n";
 }
